774f4d0957599c6ca57309bb65d4c993903d0944
Hamit Mizrak <hamitmizrak@gmail.com>
2025-05-24 12:56:50 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/EcodationFullStackSpringReact1Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationFullStackSpringReact1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcodationFullStackSpringReact1Application.class, args);
+	}
+
+}
